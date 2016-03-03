6fef6d3a29432773487b15b20e57d99456131293
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-03 15:46:14 -0500

    finished problem 2

--- js/main.js
+++ js/main.js
@@ -14,9 +14,49 @@ var ans1 = document.querySelector('.wrapper div:first-child .answer');
 
 ans1.textContent = avg;
 
+///////////////////////////////////
+
+
+//get an array of items that cost between $14.00 and $18.00 USD
+
+// function filterLongWords(words, i){
+//     return words.filter(function(currentWord) {
+//       return currentWord.length > i;
+//     });
+// }
+
+
+// function charFreq(string){
+//     return string.split('').reduce(function(memo, char){
+//       if (memo[char]) {
+//         memo[char] = memo[char] + 1;
+//       } else {
+//         memo[char] = 1;
+//       }
+//       return memo;
+//     }, {});
+
+var midPriceArray = items.filter(function(item) {
+	if (14 < item['price'] && item['price'] < 18)
+	return item;
+
+}).map(function(item){
+	return item['title'];
+});
+
+var ans2 = document.querySelector('.wrapper div:nth-child(2) .answer');
+ans2.textContent = midPriceArray;
+
 
 
 
 ///////////////////////////////////
+
+//Which item has a "GBP" currency code? Display it's name and price.
+
+
+
+
+
 
 
