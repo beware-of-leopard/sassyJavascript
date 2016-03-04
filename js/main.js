bbc968ace62d777565e33283e6ac1072cb15cafd
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-04 15:17:52 -0500

    dry-ed up the code a bit and finished styling.

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ var total = items.reduce(function(memo, item){
 
 // var avg = total / items.length;
 
-var avg = (Math.round((total / items.length) * 100) / 100);
+var avg = "$" + (Math.round((total / items.length) * 100) / 100);
 var ans1 = document.querySelector('.wrapper div:first-child .answer');
 
-ans1.textContent = avg;
+ans1.innerHTML = "<li>Average Price: "+avg+"</li>";
 
 ///////////////////////////////////
 
@@ -36,18 +36,27 @@ ans1.textContent = avg;
 //       return memo;
 //     }, {});
 
-var midPriceArray = items.filter(function(item) {
-	if (14 < item['price'] && item['price'] < 18)
-	return item;
+// var midPriceArray = items.filter(function(item) {
+// 	if (14 < item['price'] && item['price'] < 18)
+// 	return item;
 
-}).map(function(item){
-	return item['title'];
+// }).map(function(item){
+// 	return item['title'];
+// });
+
+
+// var ans2 = document.querySelector('.wrapper div:nth-child(2) .answer');
+// ans2.textContent = midPriceArray;
+
+var midPriceArray = items.filter(function(item) {
+	return 14 < item['price'] && item['price'] < 18;
+
+	}).map(function(item){
+		return "<li>" + item['title'] + "</li>";
 });
 
 var ans2 = document.querySelector('.wrapper div:nth-child(2) .answer');
-ans2.textContent = midPriceArray;
-
-
+ans2.innerHTML = midPriceArray.join('');
 
 
 ///////////////////////////////////
@@ -55,32 +64,39 @@ ans2.textContent = midPriceArray;
 //Which item has a "GBP" currency code? Display it's name and price.
 
 var gbpArray = items.filter(function(item) {
-	if (item["currency_code"] === "GBP")
-	return item;
+	return item["currency_code"] === "GBP";
 
 }).map(function(item){
-	return "Title: " + item['title'] + ", Price: " + item['price'];
+	return "<li><strong>Title:</strong> " + item['title'] + ", Price: &pound" + item['price'] + "</li>";
 });
 
 var ans3 = document.querySelector('.wrapper div:nth-child(3) .answer');
-ans3.textContent = gbpArray;
+ans3.innerHTML = gbpArray;
 
+// var gbpArray = items.filter(function(item) {
+// 	if (item["currency_code"] === "GBP")
+// 	return item;
 
+// }).forEach(function(item){
+// 	return "Title: " + item['title'] + ", Price: " + item['price'];
+// });
+
+// var ans3 = document.querySelector('.wrapper div:nth-child(3) .answer');
+// ans3.textContent = gbpArray;
 
 ///////////////////////////////////
 
 //Display a list of all items who are made of wood.
 
 var woodenItems = items.filter(function(item){
-	if (item.materials.indexOf('wood') > -1) {
-		return item;
-	}
+	return item.materials.indexOf('wood') > -1;
+
 }).map(function(item){
-	return " Title: " + item['title'] + " ";
+	return "<li><strong>Title:</strong> " + item['title'] + "</li>";
 });
 
 var ans4 = document.querySelector('.wrapper div:nth-child(4) .answer');
-ans4.textContent = woodenItems;
+ans4.innerHTML = woodenItems.join('');
 
 
 
@@ -90,15 +106,16 @@ ans4.textContent = woodenItems;
 
 
 var eightOrMoreMaterials = items.filter(function(item){
-	if (item.materials.length >= 8) {
-		return item;
-	}
+	return item.materials.length >= 8;
+
 }).map(function(item){
-	return " Title: " + item['title'] + " is made of " + item['materials'].length + " items. The items are " + item['materials'];
+	return "<ul><li><strong>Title:</strong> " + item['title'] + " is made of " + item['materials'].length + " items.</li><li><strong>The items are:</strong> " + item['materials'].reduce(function(memo, material){
+		return memo += "<li>" + material + "</li>";
+	},"") + "</li></ul>";
 });
 
 var ans5 = document.querySelector('.wrapper div:nth-child(5) .answer');
-ans5.textContent = eightOrMoreMaterials;
+ans5.innerHTML = eightOrMoreMaterials.join('');
 
 
 
@@ -107,12 +124,10 @@ ans5.textContent = eightOrMoreMaterials;
 //How many items were made by their sellers?
 
 var madeBySellers = items.filter(function(item){
-	if (item["who_made"] === "i_did") {
-		return item;
-	}
+	return item["who_made"] === "i_did";
 })
 
 var ans6 = document.querySelector('.wrapper div:nth-child(6) .answer');
-ans6.textContent = madeBySellers.length + " items were made by their sellers.";
+ans6.innerHTML = "<li>" + madeBySellers.length + " items were made by their sellers.</li>";
 
 
