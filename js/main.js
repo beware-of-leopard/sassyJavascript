bd1336ee5f47204d4ede05a42f474c87b5555f81
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-04 15:49:27 -0500

    dry-ed up problem 1.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 //Show me how to calculate the average price of all items.
 
-//The average price is $23.63
-
-
-var total = items.reduce(function(memo, item){
-			return memo += item["price"];
-		}, 0);
-
-// var avg = total / items.length;
-
-var avg = "$" + (Math.round((total / items.length) * 100) / 100);
 var ans1 = document.querySelector('.wrapper div:first-child .answer');
 
-ans1.innerHTML = "<li>Average Price: "+avg+"</li>";
+function getAvgPrice(array){
+
+	var avg = array.reduce(function(total, item){
+				return total + item['price'];
+			}, 0) / array.length;
+
+	return (Math.round((avg) * 100) / 100);
+}
+
+ans1.innerHTML = "<li>Average Price: $"+getAvgPrice(items)+"</li>";
 
 ///////////////////////////////////
 
